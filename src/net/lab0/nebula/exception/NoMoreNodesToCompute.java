6f37beb1311b2a7e74bdf24729291f4a443ddd47
uuh116 <uuh116@333f4dc9-c705-684f-d081-88386eedeb15>
2012-08-12 10:59:30 +0000

    Graphic Stats
    git-svn-id: http://uuh116.googlecode.com/svn/Nebulabrot@156 333f4dc9-c705-684f-d081-88386eedeb15

---
+++ src/net/lab0/nebula/exception/NoMoreNodesToCompute.java
@@ -0,0 +1,12 @@
+package net.lab0.nebula.exception;
+
+public class NoMoreNodesToCompute
+extends Exception
+{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+}
