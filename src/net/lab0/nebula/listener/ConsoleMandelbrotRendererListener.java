2ce4cf35c0f969eae2f97e73c20e292f266f0ae4
uuh116 <uuh116@333f4dc9-c705-684f-d081-88386eedeb15>
2013-08-18 18:26:37 +0000

    Added display step parameter.
    git-svn-id: http://uuh116.googlecode.com/svn/Nebulabrot@268 333f4dc9-c705-684f-d081-88386eedeb15

--- src/net/lab0/nebula/listener/ConsoleMandelbrotRendererListener.java
+++ src/net/lab0/nebula/listener/ConsoleMandelbrotRendererListener.java
@@ -11,14 +11,25 @@ public class ConsoleMandelbrotRendererListener
 implements MandelbrotRendererListener
 {
     private double lastPercent   = -1.0d;
+    private double displayStep   = 0.01;                     // 1%
     DecimalFormat  decimalFormat = new DecimalFormat("#0.0");
+    
+    public ConsoleMandelbrotRendererListener()
+    {
+        
+    }
+    
+    public ConsoleMandelbrotRendererListener(double displayStep)
+    {
+        this.displayStep = displayStep;
+    }
     
     @Override
     public void rendererProgress(long current, long total)
     {
         // System.out.println("Render at " + current + " of " + total);
         
-        if ((double) current / (double) total > lastPercent + 0.001)
+        if ((double) current / (double) total > lastPercent + displayStep)
         {
             lastPercent = (double) current / (double) total;
             System.out.println("Render at " + decimalFormat.format(lastPercent * 100.0) + "% (" + current + " of " + total + ")");
