9f8e79eb599b4fbdd309aa97bb92a998bafe0252
uuh116 <uuh116@333f4dc9-c705-684f-d081-88386eedeb15>
2013-08-18 17:57:34 +0000

    Added methods to compute the number of iteration instead of telling if the point is inside or outside the mandelbrot set. Documentation updates.
    git-svn-id: http://uuh116.googlecode.com/svn/Nebulabrot@263 333f4dc9-c705-684f-d081-88386eedeb15

--- src/net/lab0/nebula/core/MandelbrotComputeRoutines.java
+++ src/net/lab0/nebula/core/MandelbrotComputeRoutines.java
@@ -1,5 +1,11 @@
 package net.lab0.nebula.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import net.lab0.tools.HumanReadable;
+import net.lab0.tools.Pair;
+
 import org.apache.commons.lang3.time.StopWatch;
 
 /**
@@ -16,12 +22,16 @@ import org.apache.commons.lang3.time.StopWatch;
 public class MandelbrotComputeRoutines
 {
     /**
-     * Method used as a reference to check if optimized methods are valid
+     * Method used as a reference to check if optimized methods are valid.
+     * 
      * 
      * @param real
+     *            The real part coordinate of the point to compute.
      * @param img
+     *            The imaginary part coordinate of the point to compute.
      * @param maxIter
-     * @return
+     *            The maximum number of iterations to do.
+     * @return <code>true</code> if the point is outside the Mandelbrot set, <code>false</code> otherwise.
      */
     public static boolean isOutsideMandelbrotSetReference(double real, double img, int maxIter)
     {
@@ -30,7 +40,7 @@ public class MandelbrotComputeRoutines
         double real2, img2;
         
         int iter = 0;
-        while ((iter < maxIter) && ((real1 * real1 + img1 * img1) < 4))
+        while ((iter < maxIter) && ((real1 * real1 + img1 * img1) < 4.0d))
         // there is no sqrt in the non optim methods to avoids failures due to rounding errors
         {
             real2 = real1 * real1 - img1 * img1 + real;
@@ -46,6 +56,108 @@ public class MandelbrotComputeRoutines
     }
     
     /**
+     * Method used as a reference to check if optimized methods are valid.
+     * 
+     * 
+     * @param real
+     *            The real part coordinate of the point to compute.
+     * @param img
+     *            The imaginary part coordinate of the point to compute.
+     * @param maxIter
+     *            The maximum number of iterations to do.
+     * @return The number of iterations needed to know whether this point is outside Mandelbrot's set.
+     */
+    public static int computeIterationsCountReference(double real, double img, int maxIter)
+    {
+        double real1 = real;
+        double img1 = img;
+        double real2, img2;
+        
+        int iter = 0;
+        while ((iter < maxIter) && ((real1 * real1 + img1 * img1) < 4.0d))
+        // there is no sqrt in the non optim methods to avoids failures due to rounding errors
+        {
+            real2 = real1 * real1 - img1 * img1 + real;
+            img2 = 2 * real1 * img1 + img;
+            
+            real1 = real2;
+            img1 = img2;
+            
+            iter++;
+        }
+        
+        return iter;
+    }
+    
+    /**
+     * Method to use for debug purposes. Computes the values of the real part and the imaginary part for a given number of iterations. Stops when the result of
+     * the real part or the imaginary part becomes NaN.
+     * 
+     * @param real
+     *            The real part coordinate of the point to compute.
+     * @param img
+     *            The imaginary part coordinate of the point to compute.
+     * @param iterations
+     *            The quantity of iterations to do.
+     * @return a <code>List<Pair<real,imag>></code> containing the values of the real part and the imaginary part for each iteration. The first element of the
+     *         array is the first element that was computed in the loop.
+     */
+    public static List<Pair<Double, Double>> computeIterationsCountReferenceDebug(double real, double img, int iterations)
+    {
+        double real1 = real;
+        double img1 = img;
+        double real2, img2;
+        
+        ArrayList<Pair<Double, Double>> result = new ArrayList<>(iterations);
+        
+        for (int i = 0; i < iterations; ++i)
+        {
+            result.add(new Pair<Double, Double>(real1, img1));
+            real2 = real1 * real1 - img1 * img1 + real;
+            img2 = 2 * real1 * img1 + img;
+            
+            real1 = real2;
+            img1 = img2;
+            
+            if (Double.isNaN(real1) || Double.isNaN(img1))
+            {
+                break;
+            }
+        }
+        
+        return result;
+    }
+    
+    /**
+     * Same as <code>computeIterationsCountReference()</code> with an optimization to test if inside or outside every 2 iterations.
+     */
+    public synchronized static int computeIterationsCountOptim2(double real, double img, int maxIter)
+    {
+        double realsqr = real * real;
+        double imgsqr = img * img;
+        
+        double real1 = real;
+        double img1 = img;
+        double real2, img2;
+        
+        int iter = 0;
+        while ((iter < maxIter) & ((realsqr + imgsqr) < 4.0d))
+        {
+            real2 = real1 * real1 - img1 * img1 + real;
+            img2 = 2 * real1 * img1 + img;
+            
+            realsqr = real2 * real2;
+            imgsqr = img2 * img2;
+            real1 = realsqr - imgsqr + real;
+            img1 = 2 * real2 * img2 + img;
+            
+            iter += 2;
+        }
+        
+        return iter;
+    }
+    
+    /**
      * Tells is a point can be considered outside of the Mandelbrot set with the given limit.
      * 
      * @param real
@@ -54,7 +166,7 @@ public class MandelbrotComputeRoutines
      *            The imaginary part coordinate of the point to compute.
      * @param maxIter
      *            The maximum number of iterations to do.
-     * @return <code>true</code> if the point is outside the mandelbrot set, <code>false</code> otherwise.
+     * @return <code>true</code> if the point is outside the Mandelbrot set, <code>false</code> otherwise.
      */
     public static boolean isOutsideMandelbrotSet(double real, double img, int maxIter)
     {
@@ -68,7 +180,7 @@ public class MandelbrotComputeRoutines
         
         int iter = 0;
         // optim visible on non optim methods: single & avoids double branching
-        while ((iter < maxIter) & ((realsqr + imgsqr) < 4))
+        while ((iter < maxIter) & ((realsqr + imgsqr) < 4.0d))
         {
             real2 = real1 * real1 - img1 * img1 + real;
             img2 = 2 * real1 * img1 + img;
@@ -85,15 +197,7 @@ public class MandelbrotComputeRoutines
     }
     
     /**
-     * Tells is a point can be considered outside of the Mandelbrot set with the given limit. Computes by sets of 2 iterations.
-     * 
-     * @param real
-     *            The real part coordinate of the point to compute.
-     * @param img
-     *            The imaginary part coordinate of the point to compute.
-     * @param maxIter
-     *            The maximum number of iterations to do.
-     * @return <code>true</code> if the point is outside the mandelbrot set, <code>false</code> otherwise.
+     * Same as <code>isOutsideMandelbrotSet()</code> with an optimization to test if inside or outside every 2 iterations.
      */
     public static boolean isOutsideMandelbrotSetOptim2(double real, double img, int maxIter)
     {
@@ -105,7 +209,7 @@ public class MandelbrotComputeRoutines
         double real2, img2;
         
         int iter = 0;
-        while ((iter < maxIter) & ((realsqr + imgsqr) < 4))
+        while ((iter < maxIter) & ((realsqr + imgsqr) < 4.0d))
         {
             real2 = real1 * real1 - img1 * img1 + real;
             img2 = 2 * real1 * img1 + img;
@@ -122,15 +226,7 @@ public class MandelbrotComputeRoutines
     }
     
     /**
-     * Tells is a point can be considered outside of the Mandelbrot set with the given limit. Computes by sets of 4 iterations.
-     * 
-     * @param real
-     *            The real part coordinate of the point to compute.
-     * @param img
-     *            The imaginary part coordinate of the point to compute.
-     * @param maxIter
-     *            The maximum number of iterations to do.
-     * @return <code>true</code> if the point is outside the mandelbrot set, <code>false</code> otherwise.
+     * Same as <code>isOutsideMandelbrotSet()</code> with an optimization to test if inside or outside every 4 iterations.
      */
     public static boolean isOutsideMandelbrotSetOptim4(double real, double img, int maxIter)
     {
@@ -142,7 +238,7 @@ public class MandelbrotComputeRoutines
         double real2, img2;
         
         int iter = 0;
-        while ((iter < maxIter) & ((realsqr + imgsqr) < 4))
+        while ((iter < maxIter) & ((realsqr + imgsqr) < 4.0d))
         {
             real2 = real1 * real1 - img1 * img1 + real;
             img2 = 2 * real1 * img1 + img;
@@ -166,6 +262,7 @@ public class MandelbrotComputeRoutines
     
     public static void main(String[] args)
     {
+        System.out.println("Start");
         int maxIter = 2 << 10;
         int side = 2048;
         double step = 4.0 / side;
@@ -211,6 +308,23 @@ public class MandelbrotComputeRoutines
         }
         stopWatch.stop();
         System.out.println("4-Opt:" + stopWatch.toString());
+        stopWatch.reset();
+        
+        stopWatch.start();
+        long totalIterationCount = 0;
+        for (int x = 0; x < side; ++x)
+        {
+            for (int y = 0; y < side; ++y)
+            {
+                double real = -2.0 + x * step;
+                double img = -2.0 + y * step;
+                totalIterationCount += MandelbrotComputeRoutines.computeIterationsCountOptim2(real, img, maxIter);
+            }
+        }
+        
+        totalIterationCount *= 16; // multiply by the estimation of the floating point operations in 1 loop
+        stopWatch.stop();
+        System.out.println("Computing power: ~" + HumanReadable.humanReadableNumber(1000L * totalIterationCount / stopWatch.getTime(), true) + "flops");
         stopWatch.reset();
     }
 }
