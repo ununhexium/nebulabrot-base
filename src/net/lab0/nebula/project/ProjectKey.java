ab1cd76faed30f10315be190aa75a9a4c05ce72c
uuh116 <uuh116@333f4dc9-c705-684f-d081-88386eedeb15>
2013-08-25 08:17:44 +0000

    Added project class and test
    git-svn-id: http://uuh116.googlecode.com/svn/Nebulabrot@271 333f4dc9-c705-684f-d081-88386eedeb15

---
+++ src/net/lab0/nebula/project/ProjectKey.java
@@ -0,0 +1,16 @@
+package net.lab0.nebula.project;
+
+public enum ProjectKey
+{
+    QUAD_TREE_FOLDER("tree"),
+    ;
+    
+    public final String keyName;
+
+    private ProjectKey(String keyName)
+    {
+        this.keyName = keyName;
+    }
+    
+    
+}
