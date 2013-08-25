6578b1ca81b7aa24aee43eeda2e720f4f9e3f9e6
uuh116 <uuh116@333f4dc9-c705-684f-d081-88386eedeb15>
2013-08-25 08:26:48 +0000

    added example file
    git-svn-id: http://uuh116.googlecode.com/svn/Nebulabrot@272 333f4dc9-c705-684f-d081-88386eedeb15

---
+++ src/net/lab0/nebula/example/Example1.java
@@ -0,0 +1,20 @@
+package net.lab0.nebula.example;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import net.lab0.nebula.project.Project;
+
+public class Example1
+{
+    public static void main(String[] args)
+    throws Exception
+    {
+        Path path = FileSystems.getDefault().getPath("F:", "dev", "nebula", "project");
+        /*
+         * Create a new project item. This will store save and load the configuration of the project we are going to
+         * work on.
+         */
+        Project project = new Project(path);
+    }
+}
