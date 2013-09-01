b78ff89b60dfc5d306bbe0b7a3f18809bb5c35c0
uuh116 <uuh116@333f4dc9-c705-684f-d081-88386eedeb15>
2013-09-01 06:30:26 +0000

    RenderingMethod doc Indexing typos/grammar correction
    git-svn-id: http://uuh116.googlecode.com/svn/Nebulabrot@286 333f4dc9-c705-684f-d081-88386eedeb15

--- src/net/lab0/nebula/enums/Indexing.java
+++ src/net/lab0/nebula/enums/Indexing.java
@@ -1,9 +1,10 @@
 package net.lab0.nebula.enums;
 
 import net.lab0.nebula.core.QuadTreeManager;
+import net.lab0.nebula.project.Project;
 
 /**
- * Indicates if the {@link QuadTreeManager} have to use indexing when saving a quadtree.
+ * Indicates whether the {@link QuadTreeManager} of a {@link Project} has to use indexing when saving a quadtree.
  * 
  * @author 116@lab0.net
  * 
--- src/net/lab0/nebula/enums/RenderingMethod.java
+++ src/net/lab0/nebula/enums/RenderingMethod.java
@@ -1,5 +1,14 @@
 package net.lab0.nebula.enums;
 
+import net.lab0.nebula.data.RawMandelbrotData;
+import net.lab0.nebula.project.Project;
+
+/**
+ * Indicates which method the {@link RawMandelbrotData} of a {@link Project} has to use when doing a rendering.
+ * 
+ * @author 116@lab0.net
+ * 
+ */
 public enum RenderingMethod
 {
     LINEAR,
