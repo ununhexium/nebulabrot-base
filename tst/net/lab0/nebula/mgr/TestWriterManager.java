2b8ec16efc5b4be01700b99df516d683df44e7a1
uuh116 <uuh116@333f4dc9-c705-684f-d081-88386eedeb15>
2013-11-12 20:22:14 +0000

    Raw mandelbrot data writer test
    git-svn-id: http://uuh116.googlecode.com/svn/Nebulabrot@330 333f4dc9-c705-684f-d081-88386eedeb15

--- tst/net/lab0/nebula/mgr/TestWriterManager.java
+++ tst/net/lab0/nebula/mgr/TestWriterManager.java
@@ -4,11 +4,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.DoubleBuffer;
+import java.nio.IntBuffer;
 import java.nio.LongBuffer;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
 import net.lab0.nebula.data.PointsBlock;
+import net.lab0.nebula.data.RawMandelbrotData;
 import net.lab0.nebula.exception.SerializationException;
 
 import org.junit.Assert;
@@ -19,7 +21,7 @@ public class TestWriterManager
     private PointsBlockManager pointsBlockManager = new PointsBlockManager(1);
     
     @Test
-    public void testWrite()
+    public void testWritePointsBlock()
     throws SerializationException, IOException
     {
         PointsBlock block = new PointsBlock(1024, pointsBlockManager);
@@ -39,6 +41,7 @@ public class TestWriterManager
         Path path = FileSystems.getDefault().getPath("test", "write_manager", "test_file.data");
         path.toFile().getParentFile().mkdirs();
         writerManager.write(block, path);
+        writerManager.release(path);
         
         Assert.assertTrue("The file is empty", path.toFile().length() > 0);
         
@@ -46,9 +49,9 @@ public class TestWriterManager
         try (
             FileInputStream in = new FileInputStream(path.toFile()))
         {
-            byte[] buffer = new byte[1024 * (Double.SIZE/8)];
+            byte[] buffer = new byte[1024 * (Double.SIZE / 8)];
             ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
-            in.read(buffer, 0, Integer.SIZE/8);
+            in.read(buffer, 0, Integer.SIZE / 8);
             Assert.assertEquals(1024, byteBuffer.asIntBuffer().get());
             
             in.read(buffer);
@@ -75,6 +78,56 @@ public class TestWriterManager
             for (int i = 0; i < 1024; ++i)
             {
                 Assert.assertEquals("Error at index " + i, i, longBuffer.get());
+            }
+        }
+    }
+    
+    @Test
+    public void testWriteRawMandelbrotData()
+    throws SerializationException, IOException
+    {
+        int W = 64;
+        int H = 128;
+        RawMandelbrotData mandelbrotData = new RawMandelbrotData(W, H, 0);
+        int[][] data = mandelbrotData.getData();
+        for (int x = 0; x < W; ++x)
+        {
+            for (int y = 0; y < H; ++y)
+            {
+                data[x][y] = y + H * x;
+            }
+        }
+        
+        WriterManager writerManager = new WriterManager();
+        Path output = FileSystems.getDefault().getPath("test", "write_manager", "test_file2.data");
+        output.toFile().getParentFile().mkdirs();
+        writerManager.write(mandelbrotData, output);
+        writerManager.release(output);
+        
+        Assert.assertTrue("The file is empty", output.toFile().length() > 0);
+        try (
+            FileInputStream in = new FileInputStream(output.toFile()))
+        {
+            byte[] buffer = new byte[Integer.SIZE / 8 * H];
+            ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
+            in.read(buffer, 0, Integer.SIZE / 8 * 2);
+            Assert.assertEquals(W, byteBuffer.asIntBuffer().get(0));
+            Assert.assertEquals(H, byteBuffer.asIntBuffer().get(1));
+            
+            byteBuffer.rewind();
+            IntBuffer intBuffer = byteBuffer.asIntBuffer();
+            int current = 0;
+            int read = 0;
+            while ((read = in.read(buffer)) >= 0)
+            {
+                intBuffer.rewind();
+                for (int i = 0; i < read * 8 / Integer.SIZE; ++i)
+                {
+                    int tested = intBuffer.get();
+                    Assert.assertEquals("Error at " + i + " expected " + current + " but was " + tested, current,
+                    tested);
+                    current++;
+                }
             }
         }
     }
